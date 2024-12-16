3ea17ef0c98637a839053867b0fc15aaa98fc386
prevetal <31705823+prevetal@users.noreply.github.com>
2024-12-16 11:01:47 +0000

    Deploying to build from @ bro-n-bro/bust_or_boom_beta@275ce641450c19342ed8fabfe3b306273d6b9ba3 🚀

--- js/jetPack.js
+++ js/jetPack.js
@@ -172,7 +172,7 @@
             try {
                 // Open the URL
                 this._openUrl(`https://t.me/${BOT_USERNAME}/${APP_NAME}?startapp=${encodedData}`)
-                // this._openUrl(`http://localhost:8081/auth?tgWebAppStartParam=${encodedData}`)
+                // this._openUrl(`http://localhost:8080/auth?tgWebAppStartParam=${encodedData}`)
 
                 // Connection
                 this._connection()
